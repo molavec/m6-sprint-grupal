199154f29f6a94ac86d965314b756e16ddbb430e
Miguel Olave <molavec@gmail.com>
2023-03-14 16:52:07 -0300

    saca las funciones a una libreria

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const bodyparser = require('body-parser');
 
 const equiposObj = require('./data/equipos.json');
 const carrerasObj =  require('./data/carreras.json');
-const resultadosExample =  require('./data/resultados');
 
 const queries = require('./lib/queries');
+const functions =  require('./lib/functions');
 
 
 queries.createDatabase();
@@ -23,68 +23,6 @@ equiposObj.equipos.forEach(equipo => {
   pilotosList.push({ piloto: equipo.piloto1, escuderia: equipo.escuderia });
   pilotosList.push({ piloto: equipo.piloto2, escuderia: equipo.escuderia });
 });
-
-// obtiene los resultados por piloto
-
-const resultadosPorPiloto = (pilotos, resultados) => {
-  
-  // console.log('resultados', resultados)
-
-  const resultadosPilotos = pilotos.map( (piloto) => {
-    
-    // Nombre del piloto
-    const nombre = piloto.piloto;
-    
-    // puntajes del piloto para cada circuito
-    const puntajes = [];
-    puntajes.length = carrerasObj.carreras.length;
-    
-    // Puntaje acumulado del piloto
-    let puntajeAcumulado = 0;
-    
-    // Completa el contenido de puntajes y puntajeAcumulado
-    resultados.forEach((resultado) => {
-      if (resultado.piloto == nombre){
-        puntajes[resultado.circuito_id - 1] = puntajePorLugar(resultado.lugar);
-        // console.log(`puntajes[] = `, puntajes);
-        puntajeAcumulado += puntajePorLugar(resultado.lugar);
-      }
-    });
-
-    return {
-      nombre: nombre,
-      puntajes: puntajes,
-      puntajeAcumulado: puntajeAcumulado,
-    }
-
-  })
-
-  // Ordena los resultados segun puntaje acumulado
-  const sortedResults = resultadosPilotos.sort((a, b)=> b.puntajeAcumulado - a.puntajeAcumulado);
-
-  // Añade el lugar en la clasificación general
-  let counter = 0;
-  sortedResults.forEach((result) => {
-    result.lugar = ++ counter;
-  })
-
-  //console.log('sortedResults',sortedResults);
-  return sortedResults;
-}
-
-const puntajePorLugar = (lugar) => {
-  if(lugar == 1 ) return 25;
-  if(lugar == 2 ) return 18;
-  if(lugar == 3 ) return 15;
-  if(lugar == 4 ) return 12;
-  if(lugar == 5 ) return 10;
-  if(lugar == 6 ) return 8;
-  if(lugar == 7 ) return 6
-  if(lugar == 8 ) return 4;
-  if(lugar == 9 ) return 2;
-  if(lugar == 10 ) return 1;
-  return 0;
-}
 
 
 // configura el engine de template para que funcione con express.
@@ -104,7 +42,7 @@ app.get('/', (req, res) => {
 
   res.render('home', {
     carreras: carrerasObj.carreras, 
-    puntajes: resultadosPorPiloto(pilotosList, resultados),
+    puntajes: functions.resultadosPorPiloto(pilotosList, resultados, carrerasObj.carreras),
   });
 })
 
